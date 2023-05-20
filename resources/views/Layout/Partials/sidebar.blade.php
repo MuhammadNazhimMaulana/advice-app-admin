213ddb963e9f7d102d38d599104653253ab6c1de
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-20 21:05:45 +0700

    Update on Menu name

--- resources/views/Layout/Partials/sidebar.blade.php
+++ resources/views/Layout/Partials/sidebar.blade.php
@@ -6,7 +6,7 @@
 
     <div class="items">
         <li class="{{ ($title == "Dashboard") ? 'active' : '' }}"><i class="fas fa-tachometer-alt"></i><a href="/dashboard">Dashboard</a></li>
-        <li class="{{ ($title == "Employee") ? 'active' : '' }}"><i class="fas fa-users"></i><a href="/admin/employee">Employee</a></li>
+        <li class="{{ ($title == "Employee") ? 'active' : '' }}"><i class="fas fa-users"></i><a href="/admin/employee">Petugas Loket</a></li>
     </div>
 
 </section>
