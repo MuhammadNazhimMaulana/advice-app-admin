ff6ca282b4f30982e2e1d4d97a2fe62a0efbf8a3
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-21 18:48:38 +0700

    View Update

--- resources/views/Employee/detail.blade.php
+++ resources/views/Employee/detail.blade.php
@@ -5,6 +5,9 @@
 
     <div style="margin-top: -20px;" class="board">
 
+        {{-- Button Back --}}
+        <a class="btn btn-primary" href="/admin/employee">Back to List</a>
+
         {{-- Title --}}
         <h1 class="text-center mb-4">{{ $employee->name }}</h1>
 
