848e4158325d1284c9a9f2ea9adeed2f4a253126
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-29 20:57:17 +0700

    Adding Observer

--- app/Models/EmployeeEvaluation.php
+++ app/Models/EmployeeEvaluation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeEvaluation extends Model
 {
     use HasFactory;
+
+    const SCORE_VERY_GOOD = 5;
+    const SCORE_GOOD = 4;
+    const SCORE_NOT_BAD = 3;
+    const SCORE_BAD = 2;
+    const SCORE_REALLY_BAD = 1;
 }
